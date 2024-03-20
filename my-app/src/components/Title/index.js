82d6a83cb3976ffc710c5e2e1f07855bde85c610
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-20 15:55:21 -0300

    Primeiro componente no react native

---
+++ my-app/src/components/Title/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Title() {
+  return (
+    <View>
+      <Text style={{ color: "#96228e", fontSize: "42px", fontWeight: "700"}}>
+        OneBitHealth
+      </Text>
+    </View>
+  );
+}
